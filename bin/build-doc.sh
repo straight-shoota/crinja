aa622a13a779e25eb0bcc8cc6284bac102275d80
Johannes Müller <johannes.mueller@smj-fulda.org>
2017-05-30 20:20:49 +0000

    improve docs generation

--- bin/build-doc.sh
+++ bin/build-doc.sh
@@ -1,13 +1,23 @@
-#! /bin/sh
+#! /usr/bin/env bash
 # This script builds the latest documentation and copies it into a doc directory.
 DOC_ROOT=${1:-"_doc"}
 
-echo "Building docs: `crystal doc`"
+if [ ! -d ${DOC_ROOT} ]; then
+  echo "${DOC_ROOT} does not exist"
+  exit 1
+fi
+
+echo "Building docs with \`crystal doc\`."
 crystal doc
 echo "Done."
+echo "Moving generated docs to ${DOC_ROOT}/doc/latest"
 
-[ -d ${DOC_ROOT}/doc/latest/ ] && rm -r ${DOC_ROOT}/doc/latest/
+if [ -d ${DOC_ROOT}/doc/latest/ ]; then
+  rm -r ${DOC_ROOT}/doc/latest/
+fi
 cp -r doc/ ${DOC_ROOT}/doc/latest
 
+echo "Moving README files to doc root."
 # escape jinja2 delimiters, so Jekyll does not interpret them
 sed 's/{{/{{ "{{" }}/g; s/{%/{{ "{%" }}/g' README.md > "${DOC_ROOT}/README.md"
+sed 's/{{/{{ "{{" }}/g; s/{%/{{ "{%" }}/g' ROADMAP.md > "${DOC_ROOT}/ROADMAP.md.md"
