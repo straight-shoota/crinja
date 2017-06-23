8dad580051a5358721ca26c8735bc43d027f5a47
Johannes Müller <straightshoota@gmail.com>
2017-06-23 12:51:54 +0000

    Improve escaped experssions for README.md

--- bin/generate-docs.sh
+++ bin/generate-docs.sh
@@ -5,5 +5,6 @@ echo -e "Running crystal doc..."
 crystal doc
 
 echo -e "Copying README.md and ROADMAP.md"
-sed 's/{{/{{ "{{" }}/g; s/{%/{{ "{%" }}/g' README.md > "doc/README.md"
-sed 's/{{/{{ "{{" }}/g; s/{%/{{ "{%" }}/g' ROADMAP.md > "doc/ROADMAP.md.md"
+# "{{" and "{%"" need to be escaped, otherise Jekyll might interpret the expressions (on Github Pages)
+sed 's/{{/&#123;&#123;/g; s/{%/&#123;%/g' README.md > "doc/README.md"
+sed 's/{{/&#123;&#123;/g; s/{%/&#123;%/g' ROADMAP.md > "doc/ROADMAP.md"
