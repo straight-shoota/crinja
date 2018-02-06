6d31b723263fb2d69cd39fee094bdd5c6ef98d95
Johannes Müller <straightshoota@gmail.com>
2018-02-06 16:33:35 +0000

    Fix .travis-ci.yml and scripts/generate-docs.sh to fail CI on docs error and use ./docs as target dir

--- scripts/generate-docs.sh
+++ scripts/generate-docs.sh
@@ -1,10 +1,10 @@
-GENERATED_DOCS_DIR="./doc"
+GENERATED_DOCS_DIR="./docs"
 
 echo -e "Building docs into ${GENERATED_DOCS_DIR}"
 echo -e "Clearing ${GENERATED_DOCS_DIR} directory"
 rm -rf "${GENERATED_DOCS_DIR}"
 
-echo -e "Running crystal doc..."
+echo -e "Running `crystal docs`..."
 crystal doc
 
 echo -e "Copying README.md and ROADMAP.md"
