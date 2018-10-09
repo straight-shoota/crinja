c667ff18e0bcc241fa533046a7b08443d4db0e80
Johannes Müller <straightshoota@gmail.com>
2018-10-09 12:44:40 +0200

    Integrate JSON::Any and YAML::Any as Crinja::Object

---
+++ src/yaml.cr
@@ -0,0 +1,26 @@
+require "yaml"
+
+def Crinja.value(any : YAML::Any) : Crinja::Value
+  case raw = any.raw
+  when Hash, Array
+    value
+  else
+    value(raw)
+  end
+end
+
+def Crinja.new(any : YAML::Any) : Crinja::Value
+  value(any)
+end
+
+struct YAML::Any
+  include Crinja::Object
+
+  def crinja_attribute(attr : Crinja::Value) : Crinja::Value
+    if @raw.is_a?(Hash) || @raw.is_a?(Array)
+      result = self[attr.raw]?
+    end
+    result ||= Crinja::Undefined.new(attr.to_s)
+    return Crinja::Value.new(result)
+  end
+end
