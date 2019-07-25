dbbc6dfd773c1d32cb8d9c908d669c340fb16886
Johannes Müller <straightshoota@gmail.com>
2019-07-25 10:40:18 +0200

    Remove return type restriction on Operator#value

--- src/lib/operator.cr
+++ src/lib/operator.cr
@@ -41,7 +41,7 @@ abstract class Crinja::Operator
       true
     end
 
-    abstract def value(env : Crinja, op1 : Value, op2 : Value) : Value
+    abstract def value(env : Crinja, op1 : Value, op2 : Value)
   end
 
   module Unary
@@ -49,7 +49,7 @@ abstract class Crinja::Operator
       true
     end
 
-    abstract def value(env : Crinja, op : Value) : Value
+    abstract def value(env : Crinja, op : Value)
   end
 
   module Logic
@@ -57,7 +57,7 @@ abstract class Crinja::Operator
       true
     end
 
-    abstract def value(env : Crinja, op1 : Value, &block : -> Value) : Value
+    abstract def value(env : Crinja, op1 : Value, &block : -> Value)
   end
 end
 
