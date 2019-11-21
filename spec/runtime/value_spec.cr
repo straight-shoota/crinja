99b586cfe16058f742713020bee0ed4a9115c2be
Johannes Müller <straightshoota@gmail.com>
2019-11-21 23:15:37 +0100

    Fix Crinja::Value cast methods to raise Crinja::Error

--- spec/runtime/value_spec.cr
+++ spec/runtime/value_spec.cr
@@ -12,4 +12,29 @@ describe Crinja::Value do
       Crinja::Value.new([1, 2, "3"]).to_a.should eq([Crinja::Value.new(1), Crinja::Value.new(2), Crinja::Value.new("3")])
     end
   end
+
+  it "#as_time" do
+    time = Time.utc
+    Crinja::Value.new(time).as_time.should eq time
+
+    expect_raises Crinja::Error, "Unexpected type in Crinja value" do
+      Crinja::Value.new(nil).as_time
+    end
+
+    expect_raises Crinja::Error, "Unexpected type in Crinja value" do
+      Crinja::Value.new("time").as_time
+    end
+  end
+
+  it "#as_undefined" do
+    Crinja::Value.new(Crinja::UNDEFINED).as_undefined.should eq Crinja::UNDEFINED
+
+    expect_raises Crinja::Error, "Unexpected type in Crinja value" do
+      Crinja::Value.new(nil).as_undefined
+    end
+
+    expect_raises Crinja::Error, "Unexpected type in Crinja value" do
+      Crinja::Value.new("time").as_undefined
+    end
+  end
 end
