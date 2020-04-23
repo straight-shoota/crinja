straight-shoota/crinja

commit 3e2c224c80859d47ca7508b729bae829a493b001

Improve Makefile

Makefile
@@ -1,23 +1,60 @@
--include Makefile.local # for optional local options e.g. threads
+-include Makefile.local # for optional local options
 
-BATS ?= $(shell which bats)
-CRYSTAL ?= $(shell which crystal)
+BIN ::= bin
+SHARDS ::= shards # The shards command to use
+CRYSTAL ::= crystal # The crystal command to use
+BATS ::= bats
+
+DOCS_FLAGS ::= -o _docs
+
+SRC_SOURCES := $(shell find src lib -name '*.cr' 2>/dev/null)
+SPEC_SOURCES := $(shell find spec -name '*.cr' 2>/dev/null)
 
 .PHONY: test
-test:
+test: ## Run test suite
+test: test/unit test/integration
+
+.PHONY: test/unit
+test/unit: ## Run unit tests
+test/unit: shard.lock
 	$(CRYSTAL) spec
 
-.PHONY: integration-test
-integration-test:
+.PHONY: test/integration
+test/integration: ## Run integration tests
+test/integration:
 	$(BATS) examples/integration_test.bats
 
-docs:
-	CRYSTAL_BIN=$(CRYSTAL) scripts/generate-docs.sh
+.PHONY: format
+format: ## Apply source code formatting
+format: $(SRC_SOURCES) $(SPEC_SOURCES)
+	$(CRYSTAL) tool format src spec
 
-.PHONY: deploy-docs
-deploy-docs: docs
-	curl https://raw.githubusercontent.com/straight-shoota/autodeploy-docs/master/autodeploy-docs.sh | bash
+docs: ## Generate API docs
+docs: src/**
+	$(CRYSTAL) docs $(DOCS_FLAGS)
 
-.PHONY: clean-docs
-clean-docs:
+shard.lock: ## Update shards
+shard.lock: shard.yml
+	$(SHARDS) update
+
+.PHONY: clean
+clean: ## Remove application binary
+clean:
 	rm -rf docs
+
+.PHONY: help
+help: ## Show this help
+	@echo
+	@printf '\033[34mtargets:\033[0m\n'
+	@grep -hE '^[a-zA-Z/_-]+:.*?## .*$$' $(MAKEFILE_LIST) |\
+		sort |\
+		awk 'BEGIN {FS = ":.*?## "}; {printf "  \033[36m%-15s\033[0m %s\n", $$1, $$2}'
+	@echo
+	@printf '\033[34moptional variables:\033[0m\n'
+	@grep -hE '^[a-zA-Z_-]+ \?=.*?## .*$$' $(MAKEFILE_LIST) |\
+		sort |\
+		awk 'BEGIN {FS = " \\?=.*?## "}; {printf "  \033[36m%-15s\033[0m %s\n", $$1, $$2}'
+	@echo
+	@printf '\033[34mrecipes:\033[0m\n'
+	@grep -hE '^##.*$$' $(MAKEFILE_LIST) |\
+		awk 'BEGIN {FS = "## "}; /^## [a-zA-Z_-]/ {printf "  \033[36m%s\033[0m\n", $$2}; /^##  / {printf "  %s\n", $$2}'

.travis.yml
@@ -13,6 +13,6 @@ addons:
     - bats
 
 script:
-- make test
+- make test/unit
 - crystal tool format --check src spec
-- make integration-test
+- make test/integration

commit f86e1c59a90246191112de27a47ceff7999a6567

[CI] Fix deploy-docs on circle-ci
Was broken by 3e2c224c80859d47ca which removed deploy-docs target

.circleci/config.yml
@@ -55,9 +55,11 @@ jobs:
       - run: shards
       - save_cache: *save_shards_cache
 
+      - run: make docs
+
       - run: apt update && apt install -y curl rsync
       - run:
-          command: make clean-docs deploy-docs
+          command: curl https://raw.githubusercontent.com/straight-shoota/autodeploy-docs/master/autodeploy-docs.sh | bash
           environment:
             GIT_COMMITTER_NAME: cirlceci
             GIT_COMMITTER_EMAIL: circle@circleci.com

Makefile
@@ -28,7 +28,7 @@ format: $(SRC_SOURCES) $(SPEC_SOURCES)
 
 docs: ## Generate API docs
 docs: $(SRC_SOURCES) lib
-	$(CRYSTAL) docs
+	CRYSTAL=$(CRYSTAL) scripts/generate-docs.sh
 
 lib: shard.lock
 	$(SHARDS) install

scripts/generate-docs.sh
@@ -3,14 +3,14 @@
 set -e
 
 GENERATED_DOCS_DIR="./docs"
-CRYSTAL_BIN=${CRYSTAL_BIN:-$(which crystal)}
+CRYSTAL=${CRYSTAL:-$(which crystal)}
 
 echo -e "Building docs into ${GENERATED_DOCS_DIR}"
 echo -e "Clearing ${GENERATED_DOCS_DIR} directory"
 rm -rf "${GENERATED_DOCS_DIR}"
 
-echo -e "Running \`${CRYSTAL_BIN} docs\`..."
-${CRYSTAL_BIN} docs src/docs.cr
+echo -e "Running \`${CRYSTAL} docs\`..."
+${CRYSTAL} docs src/docs.cr
 
 echo -e "Copying README.md and TEMPLATE_SYNTAX.md"
 