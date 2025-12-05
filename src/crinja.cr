24eba04fca2f75d07106a30f05e5d79019628e69
Johannes Müller <straightshoota@gmail.com>
2025-12-05 23:10:35 +0100

    fixup

--- src/crinja.cr
+++ src/crinja.cr
@@ -23,7 +23,7 @@ class Crinja
   # parse the *template*. To parse the same template once and invoke it multiple
   # times, it needs to be created directly (using `Crinja#from_string` or
   # `Template.new`) and stored in a variable.
-  def self.render(template, variables = nil, loader = Loader.new, config = Config.new) : String
+  def self.render(template, variables = nil, loader = Loader::FileSystemLoader.new, config = Config.new) : String
     String.build do |io|
       render io, template, variables, loader, config
     end
@@ -40,7 +40,7 @@ class Crinja
   # parse the *template*. To parse the same template once and invoke it multiple
   # times, it needs to be created directly (using `Crinja#from_string` or
   # `Template.new`) and stored in a variable.
-  def self.render(io : IO, template, variables = nil, loader = Loader.new, config = Config.new)
+  def self.render(io : IO, template, variables = nil, loader = Loader::FileSystemLoader.new, config = Config.new)
     env = Crinja.new(config: config, loader: loader)
 
     env.from_string(template.to_s).render(io, variables)
